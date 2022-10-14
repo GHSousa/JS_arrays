a59d20d7e4675c9da1b3792ed0b1689e147d96e2
Gabriel Hiago de Sousa <gabrielhiago96@hotmail.com>
2022-10-14 09:48:16 -0300

    Aula 01 JS_arrays

--- JS_arrays.js
+++ JS_arrays.js
@@ -1,2 +1,29 @@
-// Arrays
+// Arrays curso
+
+//Medias 10, 6.5, 8, 7.5
+
+// let nota1 = 10;
+// let nota2 = 6.5;
+// let nota3 = 8;
+// let nota4 = 7.5;
+
+// const notas = [10, 6.5, 8, 7.5];
+
+// let media = (notas[0] + notas[1] + notas[2] + notas[3])/notas.length => MELHOR FORMA DE FAZER AS CONTAS.
+
+//  console.log(media);
+
+//ADICIONANDO ELEMENTOS EM LISTA
+
+// const notas = [10, 6.5, 8, 7.5];
+// notas.push(7); => Feita para adicionar um item dentro do arrey; 
+
+//REMOVENDO NOTAS 
+
+// const notas = [10, 7, 8, 5, 10];
+// notas.pop()
+
+// let media = (notas[0] + notas[1] + notas[2] + notas[3])/ notas.length;
+// console.log(notas);
+
 
